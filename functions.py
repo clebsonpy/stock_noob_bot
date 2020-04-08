2976077698c4a64179c629844a24b8b4ed666b32
Clebson Farias <clebson2007.farias@gmail.com>
2020-04-08 18:26:24 -0300

    Performance day add

--- functions.py
+++ functions.py
@@ -1,19 +1,21 @@
 from yahooquery import Ticker
+import datetime
 
 
 def get_stock(symbol):
     try:
         symbol = symbol+'.SA'
         ticker = Ticker(symbol)
-        data = ticker.financial_data.get(symbol)
-        value = data.get('currentPrice')
-        return value
+        df = ticker.history(period='1mo', interval='1d')
+        value = df.close[-1]
+        value_ = df.close[-2]
+        percent_day = ((value - value_)/value_)*100
+        return value, percent_day
     except AttributeError:
         raise AttributeError(ticker.financial_data)
 
-
 def main():
-    print(get_stock('PETR'))
+    print(get_stock('sula11'))
 
 
 if __name__ == '__main__':
